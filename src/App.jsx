d7c5845bfaa143f96568b456212e2a72fded96d7
Hesnan Ávila <avilahesnan@outlook.com>
2023-12-06 21:48:52 -0300

    new file: initial

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import Editor from './components/Editor';
+import Header from './components/Header';
+import Stories from './components/Stories';
+import './App.css';
+
+export default function App() {
+	const stories = [
+		{
+			id: 1, title: 'Primeira História', description:
+				'Texto da primeira história.'
+		},
+		{
+			id: 2, title: 'Segunda História', description:
+				'Texto da segunda história.'
+		},
+		{
+			id: 3, title: 'Terceira História', description:
+				'Texto da terceira história.'
+		}
+	];
+	function selectStory(story) {
+		console.log(story);
+	}
+	return (
+		<>
+			<Header />
+			<Stories stories={stories} selectStory={selectStory} />
+			<Editor />
+		</>
+	);
+}
